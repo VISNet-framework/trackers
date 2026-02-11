b6d2e74b322c1f561a2a3f464e0c193a6001957f
Bart <bart.vanmarrewijk@wur.nl>
2026-02-11 12:03:10 +0100

    another attempt

--- test/test_match_template.py
+++ test/test_match_template.py
@@ -14,7 +14,7 @@ def test_match_template():
 
 def match(movement, direction):
 
-    indir = Path(__file__).parent / "../data/belt_tulip"
+    indir = Path(__file__).parent / "../../data/belt_tulip"
     name_1 = indir / "437.png"
     name_2 = indir / "438.png"
 
